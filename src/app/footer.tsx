61e47d833e30f3e440ef2acfff46f0ee95b5278f
Lily Huang <l613426210@gmail.com>
2023-10-10 11:27:53 -0600

    fix image error

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image'
 
-import mypic from '/Users/huanglily/Desktop/Plums/plums/public/images/fb.png'
+import fb from '/public/images/fb.png'
+import ig from '/public/images/ig.png'
+import linkedIn from '/public/images/linkedin.png'
+import logo from '/public/images/logo.png'
+import pinterest from '/public/images/pinterest.png'
+import x from '/public/images/x.png'
+import youtube from '/public/images/youtube.png'
 
 
 
@@ -12,19 +18,19 @@ const Footer = () => {
         <footer className={"dark:bg-violet-500 dark:text-white bg-slate-100 text-black"}>
             <Link href={"#"}>Page</Link>
             <Link href={"#"}>Page</Link>
-            <Link  href={"Home"} >PLUMS</Link>
+            <Link href={"Home"} >PLUMS</Link>
             <Link href={"#"}>Page</Link>
             <Link href={"#"}>Page</Link>
 
             <hr></hr>
             <ul>
-                <li><Link href="/"><Image src={mypic} alt='Facebook icon' width={150} height={150} /></Link></li>
-                <li><Link href="/"><Image src="/public/images/ig.png" alt='Instagram icon' width={150} height={150} /></Link></li>
-                <li><Link href="/"><Image src="/public/images/linkedin.png" alt='Linkedin icon' width={150} height={150} /></Link></li>
-                <li><Link href="/"><Image src="#" alt='Logo' width={200} height={200} /></Link></li>
-                <li><Link href="/"><Image src="/public/images/pinterest.png" alt='Pinterest icon' width={150} height={150} /></Link></li>
-                <li><Link href="/"><Image src="/public/images/x.png" alt='X(Twitter) icon' width={150} height={150} /></Link></li>
-                <li><Link href="/"><Image src="/public/images/youtube.png" alt='Youtube icon' width={150} height={150} /></Link></li>
+                <li><Link href="/"><Image src={fb} alt='Facebook icon' width={150} height={150} /></Link></li>
+                <li><Link href="/"><Image src={ig} alt='Instagram icon' width='150' height='150' /></Link></li>
+                <li><Link href="/"><Image src={linkedIn} alt='Linkedin icon' width={150} height={150} /></Link></li>
+                <li><Link href="/"><Image src={logo} alt='Logo' width={200} height={200} /></Link></li>
+                <li><Link href="/"><Image src={pinterest} alt='Pinterest icon' width={150} height={150} /></Link></li>
+                <li><Link href="/"><Image src={x} alt='X(Twitter) icon' width={150} height={150} /></Link></li>
+                <li><Link href="/"><Image src={youtube} alt='Youtube icon' width={150} height={150} /></Link></li>
                 
             </ul>
             

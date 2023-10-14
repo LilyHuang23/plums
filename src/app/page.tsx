c461e73796a0fe5870484999c7528546db2abcea
isaacpradford <radf.isaac@gmail.com>
2023-10-14 17:18:55 -0600

    initial landing page push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import Image from 'next/image'
 import Footer from '../components/footer'
 import Header from '@/components/header'
 
+
+import Pricing from '@/components/landingPage/pricing'
+
 export default function Home() {
   return (
     <main>
       <Header />
+      <Pricing />
       {/* <Footer /> */}
-      
     </main>
   )
 }
---
+++ src/components/landingPage/pricing.tsx
@@ -0,0 +1,16 @@
+export default function Pricing() {
+
+    return (
+        <div className="pricing w-full h-landing-box space-x-10 bg-russian-violet">
+            <div className="card bg-white w-72 h-96">
+
+            </div>
+
+            <div className="card bg-white w-80 h-100">
+            </div>
+
+            <div className="card bg-white w-72 h-96">
+            </div>
+        </div>
+    )
+}

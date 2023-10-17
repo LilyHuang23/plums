04f59abb4a2189fdbba7603f1268a44592129477
isaacpradford <radf.isaac@gmail.com>
2023-10-17 11:13:35 -0600

    Final pricing

--- src/components/landingPage/pricing.tsx
+++ src/components/landingPage/pricing.tsx
@@ -3,7 +3,7 @@ export default function Pricing() {
     return (
         <div className="pricing w-full h-landing-box space-x-10 bg-menu-white">
                 <div className="card bg-wisteria w-72 h-96">
-                    <h1 className="text-4xl py-3">FREE</h1>
+                    <h1 className="text-4xl py-3 text-menu-white">FREE</h1>
                     <h2 className="text-2xl">BASIC</h2>
                     <div className="internal_card text-lg">
                         <ul className="py-3">
@@ -16,7 +16,7 @@ export default function Pricing() {
                 </div>
 
                 <div className="card bg-wisteria w-80 h-100">
-                    <h1 className="text-4xl py-3">79$/mo</h1>
+                    <h1 className="text-4xl py-3 text-menu-white" >79$/mo</h1>
                     <h2 className="text-2xl text-russian-violet">PREMIUM</h2>
                     <div className="internal_card text-lg">
                         <ul className="py-3">
@@ -29,7 +29,7 @@ export default function Pricing() {
                 </div>
 
                 <div className="card bg-wisteria w-72 h-96">
-                    <h1 className="text-4xl py-3">499$/mo</h1>
+                    <h1 className="text-4xl py-3 text-menu-white">499$/mo</h1>
                     <h2 className="text-2xl text-russian-violet">ENTERPRISE</h2>
                     <div className="internal_card text-lg">
                         <ul className="py-3">

ede1f2e409071d64f539df31843b4458013f7577
Lily Huang <l613426210@gmail.com>
2023-10-18 22:39:29 -0600

    Add feature to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Header from '@/components/header'
 import Hero from '@/components/landingPage/hero'
 
 import Pricing from '@/components/landingPage/pricing'
+import Feature from '@/components/landingPage/feature'
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <Feature />
       <Pricing />
       {/* <Footer /> */}
     </main>
--- src/components/landingPage/hero.tsx
+++ src/components/landingPage/hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <p className='p-5'>With Plums’s mobile learning software, you can learn anywhere, any time
                 – whether it’s on an airplane, in another country, or in the field. Plums gives
                 learners immediate and flexible access to notes for increased engagement and knowledge retention.</p>
-            <a href="#_" className="relative inline-flex p-5 ml-5 items-center px-12 py-3 overflow-hidden text-lg font-medium text-russian-violet border-2 border-russian-violet rounded-full hover:text-white group hover:bg-gray-50">
+            <a href="#_" className="relative inline-flex p-5 ml-5 items-center px-12 py-3 overflow-hidden text-lg font-medium text-white border-2 border-white rounded-full hover:text-white group hover:bg-gray-50">
                 <span className="absolute left-0 block w-full h-0 transition-all bg-tekhelet opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
                 <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>

88726a4e5306a749a66660b4f63d9e04e98dba92
isaacpradford <radf.isaac@gmail.com>
2023-10-24 20:44:41 -0600

    Home page initial commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Footer from '@/components/footer'
+import Header from '@/components/header'
+import Hero from '@/components/landingPage/hero'
+
+import Pricing from '@/components/landingPage/pricing'
+
+export default function Home() {
+    return (
+      <main>
+        <Header />
+      </main>
+    )
+  }
+  

a4b5d0a18ddc7f0c196fe8bd6d574c7bdd581940
Lily Huang <l613426210@gmail.com>
2023-10-30 20:52:43 -0600

    create topic page

---
+++ src/app/topics/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Card from '@/components/card'
+import SideBar from '@/components/sidebar'
+
+export default function Home() {
+  return (
+    <main>
+      <SideBar />
+      <Card />
+    </main>
+  )
+}
+
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,7 +8,7 @@ import logo from '/public/images/logo.png'
 const Card = () => {
     return (
         <div className="shadow-slate-50 rounded-md">
-            <Image src="logo.png" alt="Topic" className="rounded-t-md" />
+            <Image src={logo} alt="Topic" className="rounded-t-md" />
                 <div className="padding: 2px 16px;">
                     <h4><b>Topic</b></h4>
                     <p>Create: 10/21/2023</p>

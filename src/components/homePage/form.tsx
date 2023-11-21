c5c1fc1996658a9d2d71b45ad4f5bf280c86cec7
Lily Huang <l613426210@gmail.com>
2023-11-21 10:48:37 -0700

    base64

--- src/components/homePage/form.tsx
+++ src/components/homePage/form.tsx
@@ -1,107 +1,142 @@
 'use client'
 import { useState, useRef } from "react";
 import Input from "./input";
+import { document } from "postcss";
 
 // https://www.telerik.com/blogs/how-to-programmatically-add-input-fields-react-forms
 // https://codepen.io/arefeh_htmi/pen/mdPYZKJ?editors=1100
 
 interface FormValue {
-    label: string;
-    type: string;
-    value: string;
+  label: string;
+  type: string;
+  value: string;
+}
+
+export default function Form() {
+  const [formValues, setFormValues] = useState<FormValue[]>([]);
+  const [toggle, setToggle] = useState(false);
+
+  const inputRef = useRef();
+  const selectRef = useRef();
+
+  const handleChange = (e, index) => {
+    const values = [...formValues];
+    values[index].value = e.target.value;
+    setFormValues(values);
+  };
+
+  const handleAddField = (e) => {
+    e.preventDefault();
+    const values = [...formValues];
+    values.push({
+      label: inputRef.current?.value || "label",
+      type: selectRef.current?.value || "text",
+      value: "",
+    });
+    setFormValues(values);
+    setToggle(false);
+  };
+
+
+  const handleDeleteField = (e, index) => {
+    const values = [...formValues];
+    values.splice(index, 1);
+    setFormValues(values);
   }
 
-export default function Form()
-{
-    const [formValues, setFormValues] = useState<FormValue[]>([]);
-    const [toggle, setToggle] = useState(false);
+  const addBtnClick = (e) => {
+    e.preventDefault();
+    setToggle(true);
+  }
 
-    const inputRef = useRef();
-    const selectRef = useRef();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(e);
+    // console.log(
+    //   formValues.map((val) => {
+    //     return { [val.label]: val.value };
+    //   })
+    // );
+    // Create a new array to store updated form values
+    const updatedFormValues = [];
 
-    const handleChange = (e, index) => {
-        const values = [...formValues];
-        values[index].value = e.target.value;
-        setFormValues(values);
-      };
-    
-      const handleAddField = (e) => {
-        e.preventDefault();
-        const values = [...formValues];
-        values.push({
-          label: inputRef.current?.value || "label",
-          type: selectRef.current?.value || "text",
-          value: "",
-        });
-        setFormValues(values);
-        setToggle(false);
-      };
+    // Iterate through formValues to handle file conversion
+    for (const val of formValues) {
+      if (val.type === 'file' && val.value) {
+        // If the type is 'file' and a file is selected
+        const file = val.value;
 
-      const handleDeleteField = (e, index) => {
-        const values = [...formValues];
-        values.splice(index, 1);
-        setFormValues(values);
+        // Convert file to Base64
+        const base64 = await convertFileToBase64(file);
+
+        // Store the Base64 value in the updatedFormValues array
+        updatedFormValues.push({ ...val, value: base64 });
+      } else {
+        // For other types of form fields, keep the values as they are
+        updatedFormValues.push(val);
       }
+    }
 
-      const addBtnClick = (e) => {
-        e.preventDefault();
-        setToggle(true);
-      }
+    // Log or perform actions with updatedFormValues
+    console.log(updatedFormValues);
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e);
-        console.log(
-          formValues.map((val) => {
-            return { [val.label]: val.value };
-          })
-        );
-      };
+    // Here you can proceed with sending the form data
+  };
 
-    return (
-        <div className="form_body bg-wisteria">
-            <form onSubmit={handleSubmit}>
-              <div className="input-group">
-                  <label htmlFor="title">Title: </label>
-                  <input type="text" placeholder="Title:" name="title" id="title"/>
-                </div>
+  // Function to convert file to Base64
+  const convertFileToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result.split(',')[1]);
+      reader.onerror = (error) => reject(error);
+    });
+  };
 
-                <div className="input-group">
-                  <label htmlFor="description">Description: </label>
-                  <input type="text" placeholder="Title:" />
-                </div>
-
-                {formValues.map((obj, index) => (
-                    <Input 
-                        key={index}
-                        objValue={obj}
-                        onChange={handleChange}
-                        index={index}
-                        deleteField={handleDeleteField}
-                    />
-                ))}
-                {!toggle ? (
-                    <div className="center">
-                        <button className="add-btn" onClick={addBtnClick}>
-                        Add new
-                        </button>
-                    </div>
-                    ) : (
-                    <div className="dialog-box">
-                        <input type="text" placeholder="label" ref={inputRef} />
-                        <select ref={selectRef} id={selectRef}>
-                            <option value="text">Notes</option>
-                            <option value="text">Links</option>
-                            <option value="file">File Input</option>
-                        </select>
-                        <button className="add-btn" onClick={handleAddField}>
-                        Add
-                        </button>
-                    </div>
-                    )}
-
-                <button type="submit" className="submit-btn">Submit</button>
-            </form>
+  return (
+    <div className="form_body bg-wisteria">
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="title">Title: </label>
+          <input type="text" placeholder="Title:" name="title" id="title" />
         </div>
-    )
+
+        <div className="input-group">
+          <label htmlFor="description">Description: </label>
+          <input type="text" placeholder="Title:" />
+        </div>
+
+        {formValues.map((obj, index) => (
+          <Input
+            key={index}
+            objValue={obj}
+            onChange={handleChange}
+            index={index}
+            deleteField={handleDeleteField}
+          />
+        ))}
+        {!toggle ? (
+          <div className="center">
+            <button className="add-btn" onClick={addBtnClick}>
+              Add new
+            </button>
+          </div>
+        ) : (
+          <div className="dialog-box">
+            <input type="text" placeholder="label" ref={inputRef} />
+            <select ref={selectRef} id={selectRef}>
+              <option value="text">Notes</option>
+              <option value="text">Links</option>
+              <option value="file">File Input</option>
+            </select>
+            <button className="add-btn" onClick={handleAddField}>
+              Add
+            </button>
+          </div>
+        )}
+
+        <button type="submit" className="submit-btn">Submit</button>
+      </form>
+    </div>
+  )
 }
---
+++ src/components/homePage/handleFile.tsx
@@ -0,0 +1,22 @@
+// 'use State'
+// // base64
+// //https://www.youtube.com/watch?v=wm5gMKuwSYk I think this is very similar to PLUMS! 
+// //https://base64.guru/developers/javascript/btoa
+// function setFile64(result: string | ArrayBuffer | null) {
+//     throw new Error("Function not implemented.");
+// }
+// const HandleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const fileInput = e.target.files && e.target.files[0];
+//     if (fileInput) {
+//         setFile64(fileInput)
+//         const reader = new FileReader();
+//         reader.readAsDataURL(fileInput);
+//         reader.onload = () => {
+//             setFile64(reader.result)
+//         }
+
+//     }
+// }
+// export default HandleFile
+
+

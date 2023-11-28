82a01348311be132e343b22136eb11340465d1e4
Lily Huang <l613426210@gmail.com>
2023-11-28 11:48:26 -0700

    comment out form and get rid of firebase

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,14 @@
 import '@/app/home.css'
 
 import Image from 'next/image'
-import Footer from '@/components/footer'
 import Header from '@/components/header'
-import Form from '@/components/homePage/form'
+// import Form from '@/components/homePage/form'
 
 export default function Home() {
   return (
     <main className='homePage w-full'>
         <Header />
-        <Form />
-        {/* <Footer /> */}
+        {/* <Form /> */}
     </main>
   )
 }

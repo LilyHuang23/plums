c0606a0dde3332731915608750506ee02a515472
Lily Huang <l613426210@gmail.com>
2023-11-30 10:31:31 -0700

    get rid of file path with @

--- src/app/topics/layout.tsx
+++ src/app/topics/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Sidebar from "../../components/sidebar";
-import Header from '@/components/header'
+
 // import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'

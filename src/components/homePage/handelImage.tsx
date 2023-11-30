380825daeb8201861e8f5647a1ace132667c9b49
isaacpradford <radf.isaac@gmail.com>
2023-11-30 11:49:29 -0700

    Updated file upload with Ignacio's recommendations

--- src/components/homePage/handelImage.tsx
+++ src/components/homePage/handelImage.tsx
@@ -1,26 +1,25 @@
-// 'use State'
+'use State'
 
-// function setImage(imageInput: File) {
-//     throw new Error("Function not implemented.");
-// }
+function setImage(imageInput: File) {
+    throw new Error("Function not implemented.");
+}
+
+function setImage64(result: string | ArrayBuffer | null) {
+    throw new Error("Function not implemented.");
+}
+// base64
+const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const imageInput = e.target.files && e.target.files[0];
+    if (imageInput) {
+        setImage(imageInput)
+        const reader = new FileReader();
+        reader.readAsDataURL(imageInput);
+        reader.onload = () => {
+            setImage64(reader.result)
+        }
+
+    }
+}
 
 
-// function setImage64(result: string | ArrayBuffer | null) {
-//     throw new Error("Function not implemented.");
-// }
-// // base64
-// const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const imageInput = e.target.files && e.target.files[0];
-//     if (imageInput) {
-//         setImage(imageInput)
-//         const reader = new FileReader();
-//         reader.readAsDataURL(imageInput);
-//         reader.onload = () => {
-//             setImage64(reader.result)
-//         }
-
-//     }
-// }
-
-
-// export default handleImage
+export default handleImage

14aaf0aa724ae9eacd4f5b087f647dc41ccc7336
isaacpradford <radf.isaac@gmail.com>
2023-12-04 15:52:04 -0700

    updated import statements

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import './home.css'
 import Image from 'next/image'
 import Header from '../../components/header'
 
-// import Form from '../../components/homePage/form'
+import Form from '../../components/homePage/form'
 
 // Toast Notifications and Popups
 import { ToastContainer } from 'react-toastify';
--- src/app/topics/page.tsx
+++ src/app/topics/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 
-import Card from '@/components/allTopicsPage/card'
-import SideBar from '@/components/sidebar'
-import Header from '@/components/header'
+import Card from '../../components/allTopicsPage/card';
+import SideBar from '../../components/sidebar';
+import Header from '../../components/header';
 
-import AllTopics from '@/components/allTopicsPage/allTopics'
+import AllTopics from '../../components/allTopicsPage/allTopics';
 
 export default function Home() {
   return (

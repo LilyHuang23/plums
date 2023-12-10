ef7586a3f32b66d882af7a5f7f9a6e1e1b84d1ba
Lily Huang <l613426210@gmail.com>
2023-12-10 16:30:08 -0700

    update the import

--- src/app/topics/[id]/page.tsx
+++ src/app/topics/[id]/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 
 import SideBar from '../../../components/sidebar';
 import Header from '../../../components/header';
-import RenderTopic from 'components/topicPage/RenderTopic';
+import RenderTopic from '../../../components/topicPage/RenderTopic';
+import UpdateForm from "../../../components/topicPage/UpdateForm";
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';

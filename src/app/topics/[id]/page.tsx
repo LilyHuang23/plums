457bd1c84e8e524d48b4a42955ecddd945455ca1
Lily Huang <l613426210@gmail.com>
2023-12-10 16:50:35 -0700

    change file name

--- src/app/topics/[id]/page.tsx
+++ src/app/topics/[id]/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import SideBar from '../../../components/sidebar';
 import Header from '../../../components/header';
 import RenderTopic from '../../../components/topicPage/RenderTopic';
-import UpdateForm from "../../../components/topicPage/UpdateForm";
+import UpdateForm from "../../../components/topicPage/updateForm";
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
